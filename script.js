b09f374703be54efc95d99278f64870fde0cc2c5
krolikowskaaleksandra <contact.akrolikowska@gmail.com>
2021-11-21 22:26:29 +0100

    sort products by name and price

--- script.js
+++ script.js
@@ -1,32 +1,45 @@
 const products = [{
         name: "blouse",
+        price: 100,
     },
     {
         name: "top",
+        price: 50,
+
     },
     {
         name: "dress",
+        price: 79,
+
     },
     {
         name: "sweater",
+        price: 80,
+
     },
     {
         name: "shirt",
+        price: 49,
+
     },
     {
         name: "skirt",
+        price: 120,
+
     },
 
 ];
 const productsList = document.getElementById("productsList");
 
 products.forEach(({
-    name
+    name,
+    price
 }) => {
     let li = document.createElement("li");
-    li.innerText = name;
+    li.innerText = name + " " + price;
     productsList.appendChild(li);
 })
+
 
 // // products.forEach((item) => {
 // //     let li = document.createElement("li");
@@ -40,12 +53,15 @@ function sortProductsAZ() {
     products.sort((a, b) => (a.name > b.name) ? 1 : -1)
     productsList.innerText = "";
     products.forEach(({
-        name
+        name,
+        price
     }) => {
         let li = document.createElement("li");
-        li.innerText = name;
+        li.innerText = name + " " + price;
         productsList.appendChild(li);
+
     })
+
 
 }
 
@@ -53,11 +69,43 @@ function sortProductsZA() {
     products.sort((b, a) => (b.name > a.name) ? -1 : 1)
     productsList.innerText = "";
     products.forEach(({
-        name
+        name,
+        price
     }) => {
         let li = document.createElement("li");
-        li.innerText = name;
+        li.innerText = name + " " + price;
         productsList.appendChild(li);
     })
 
+
+}
+
+function sortProductslow() {
+    products.sort((a, b) => (a.price > b.price) ? 1 : -1)
+    productsList.innerText = "";
+    products.forEach(({
+        name,
+        price
+    }) => {
+        let li = document.createElement("li");
+        li.innerText = name + " " + price;
+        productsList.appendChild(li);
+
+    })
+
+
+}
+
+function sortProductshigh() {
+    products.sort((b, a) => (b.price > a.price) ? -1 : 1)
+    productsList.innerText = "";
+    products.forEach(({
+        name,
+        price
+    }) => {
+        let li = document.createElement("li");
+        li.innerText = name + " " + price;
+        productsList.appendChild(li);
+
+    })
 }
