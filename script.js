f4a56254c39566862677635f8fe6561459a15ab4
krolikowskaaleksandra <contact.akrolikowska@gmail.com>
2022-01-08 15:25:11 +0100

    english version

--- script.js
+++ script.js
@@ -2,19 +2,19 @@
                         name: "Tonik normalizujący",
                         price: 29,
                         image: "images/tonik1.png",
-                        category: "toniki",
+                        category: "facialToner",
                 },
                 {
                         name: "Tonik oczyszczający",
                         price: 35,
                         image: "images/tonik2.png",
-                        category: "toniki",
+                        category: "facialToner",
                 },
                 {
                         name: "Tonik regulujący",
                         price: 22,
                         image: "images/tonik3.png",
-                        category: "toniki",
+                        category: "facialToner",
                 },
                 {
                         name: "Przeciwzmarszczkowe serum",
@@ -38,55 +38,55 @@
                         name: "Krem na dzień",
                         price: 49,
                         image: "images/krem1.png",
-                        category: "kremy",
+                        category: "cream",
                 },
                 {
                         name: "Krem przeciwzmarszczkowy",
                         price: 59,
                         image: "images/krem2.png",
-                        category: "kremy",
+                        category: "cream",
                 },
                 {
                         name: "Krem na noc",
                         price: 50,
                         image: "images/krem3.png",
-                        category: "kremy",
+                        category: "cream",
                 },
                 {
                         name: "Pianka oczyszczająca",
                         price: 39,
                         image: "images/pianka1.png",
-                        category: "pianki",
+                        category: "faceWash",
                 },
                 {
                         name: "Pianka do mycia twarzy",
                         price: 30,
                         image: "images/pianka2.png",
-                        category: "pianki",
+                        category: "faceWash",
                 },
                 {
                         name: "Delikatna pianka do twarzy",
                         price: 38,
                         image: "images/pianka3.png",
-                        category: "pianki",
+                        category: "faceWash",
                 },
                 {
                         name: "Peeling do twarzy",
                         price: 24,
                         image: "images/peeling1.png",
-                        category: "peeling",
+                        category: "faceScrub",
                 },
                 {
                         name: "Łagodzący peeling",
                         price: 29,
                         image: "images/peeling2.png",
-                        category: "peeling",
+                        category: "faceScrub",
                 },
                 {
                         name: "Peeling cukrowy",
                         price: 34,
                         image: "images/peeling3.png",
-                        category: "peeling",
+                        category: "faceScrub",
                 },
         ]
 
@@ -126,11 +126,11 @@
         displayProducts(products);
 
         const allProducts = document.getElementById("allProducts");
-        const toniki = document.getElementById("toniki");
+        const facialToner = document.getElementById("facialToner");
         const serum = document.getElementById("serum");
-        const kremy = document.getElementById("kremy");
-        const pianki = document.getElementById("pianki");
-        const peeling = document.getElementById("peeling");
+        const cream = document.getElementById("cream");
+        const faceWash = document.getElementById("faceWash");
+        const faceScrub = document.getElementById("faceScrub");
 
 
         const displayAllCategory = () => {
@@ -145,12 +145,12 @@
 
         const displayTonikiCategory = () => {
                 const findTonikiProducts = (product) => {
-                        return product.category === "toniki";
+                        return product.category === "facialToner";
                 }
                 const tonikiProducts = products.filter(findTonikiProducts);
                 productList.innerText = "";
                 displayProducts(tonikiProducts);
-                sessionStorage.setItem("category", "toniki")
+                sessionStorage.setItem("category", "facialToner")
         }
 
         const displaySerumCategory = () => {
@@ -165,41 +165,41 @@
 
         const displayKremyCategory = () => {
                 const findKremyProducts = (product) => {
-                        return product.category === "kremy";
+                        return product.category === "cream";
                 }
                 const kremyProducts = products.filter(findKremyProducts);
                 productList.innerText = "";
                 displayProducts(kremyProducts);
-                sessionStorage.setItem("category", "kremy")
+                sessionStorage.setItem("category", "cream")
         }
 
         const displayPiankiCategory = () => {
                 const findPiankiProducts = (product) => {
-                        return product.category === "pianki";
+                        return product.category === "faceWash";
                 }
                 const piankiProducts = products.filter(findPiankiProducts);
                 productList.innerText = "";
                 displayProducts(piankiProducts);
-                sessionStorage.setItem("category", "pianki")
+                sessionStorage.setItem("category", "faceWash")
         }
 
         const displayPeelingCategory = () => {
                 const findPeelingProducts = (product) => {
-                        return product.category === "peeling";
+                        return product.category === "faceScrub";
                 }
                 const peelingProducts = products.filter(findPeelingProducts);
                 productList.innerText = "";
                 displayProducts(peelingProducts);
-                sessionStorage.setItem("category", "peeling")
+                sessionStorage.setItem("category", "faceScrub")
         }
 
 
         allProducts.addEventListener("click", displayAllCategory)
-        toniki.addEventListener("click", displayTonikiCategory)
+        facialToner.addEventListener("click", displayTonikiCategory)
         serum.addEventListener("click", displaySerumCategory)
-        kremy.addEventListener("click", displayKremyCategory)
-        pianki.addEventListener("click", displayPiankiCategory)
-        peeling.addEventListener("click", displayPeelingCategory)
+        cream.addEventListener("click", displayKremyCategory)
+        faceWash.addEventListener("click", displayPiankiCategory)
+        faceScrub.addEventListener("click", displayPeelingCategory)
 
 
         const sortProducts = (products) => {
@@ -208,7 +208,7 @@
                         products.sort((a, b) => a.name.localeCompare(b.name));
                         productList.innerText = "";
                         switch (sessionStorage.getItem('category')) {
-                                case "toniki": {
+                                case "facialToner": {
                                         displayTonikiCategory();
                                         break;
                                 }
@@ -216,15 +216,15 @@
                                         displaySerumCategory();
                                         break;
                                 }
-                                case "kremy": {
+                                case "cream": {
                                         displayKremyCategory();
                                         break;
                                 }
-                                case "pianki": {
+                                case "faceWash": {
                                         displayPiankiCategory();
                                         break;
                                 }
-                                case "peeling": {
+                                case "faceScrub": {
                                         displayPeelingCategory();
                                         break;
                                 }
@@ -238,7 +238,7 @@
                         products.sort((a, b) => b.name.localeCompare(a.name));
                         productList.innerText = "";
                         switch (sessionStorage.getItem('category')) {
-                                case "toniki": {
+                                case "facialToner": {
                                         displayTonikiCategory();
                                         break;
                                 }
@@ -246,15 +246,15 @@
                                         displaySerumCategory();
                                         break;
                                 }
-                                case "kremy": {
+                                case "cream": {
                                         displayKremyCategory();
                                         break;
                                 }
-                                case "pianki": {
+                                case "faceWash": {
                                         displayPiankiCategory();
                                         break;
                                 }
-                                case "peeling": {
+                                case "faceScrub": {
                                         displayPeelingCategory();
                                         break;
                                 }
@@ -268,7 +268,7 @@
                         products.sort((a, b) => (a.price > b.price) ? 1 : -1)
                         productList.innerText = "";
                         switch (sessionStorage.getItem('category')) {
-                                case "toniki": {
+                                case "facialToner": {
                                         displayTonikiCategory();
                                         break;
                                 }
@@ -276,15 +276,15 @@
                                         displaySerumCategory();
                                         break;
                                 }
-                                case "kremy": {
+                                case "cream": {
                                         displayKremyCategory();
                                         break;
                                 }
-                                case "pianki": {
+                                case "faceWash": {
                                         displayPiankiCategory();
                                         break;
                                 }
-                                case "peeling": {
+                                case "faceScrub": {
                                         displayPeelingCategory();
                                         break;
                                 }
@@ -298,7 +298,7 @@
                         products.sort((a, b) => (a.price > b.price) ? -1 : 1)
                         productList.innerText = "";
                         switch (sessionStorage.getItem('category')) {
-                                case "toniki": {
+                                case "facialToner": {
                                         displayTonikiCategory();
                                         break;
                                 }
@@ -306,15 +306,15 @@
                                         displaySerumCategory();
                                         break;
                                 }
-                                case "kremy": {
+                                case "cream": {
                                         displayKremyCategory();
                                         break;
                                 }
-                                case "pianki": {
+                                case "faceWash": {
                                         displayPiankiCategory();
                                         break;
                                 }
-                                case "peeling": {
+                                case "faceScrub": {
                                         displayPeelingCategory();
                                         break;
                                 }
